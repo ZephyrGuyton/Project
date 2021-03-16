09833d3efbc8a8983221a260022dd54451babf2a
Zephyr Guyton <z.guyton@rollestoncollege.nz>
2021-03-16 20:45:03 +0000

    program ready for database

--- app.py
+++ app.py
@@ -1,42 +1,3 @@
-def linear_search(myItem,myList):
-    found = False 	# Set Found variable to False by default
-    position = 0 	# the starting point in the list
-    while position < len(myList) and not found:
-        if myList[position] == myItem:
-            found = True
-        position = position + 1
-    return found
-
-
-def createList():
-
-  low_stocks = ['FPH' 'ANZ' 'AFI' 'FSF' 'MEL']
-  medium_stocks = ['WBC' 'TLS' 'AIR' 'AIA' 'SPK']
-  high_stocks = ['CBD' 'FBU' 'SKC' 'KMD' 'GEO']
- 
-  stocks = [["fph", "low"],["ata", "medium"]]
-
-  print (stocks)
-
-  print (stocks[1])
-
-  item = input("Which item would you like to look for? ")
-
-  item_lower = item.lower() # this variable is used to change the input to lowercase before the search
-
-  isitfound = linear_search(item_lower,stocks)
-
-  if isitfound:
-    print("Your item is in the list")
-  else:
-    print("Your item is not in the list")
-
-
-  investor_type = ['Agressive' 'Defensive' 'Balanced' 'Concervative' 'Growth']
-  money = float(input('How much money would you like to invest? \n $'))
-
-  Time = int(input('How many years are you planning on investing? \n'))
-  moneyIn(money, Time)
 
 low_stocks = ['FPH', 'ANZ', 'AFI', 'FSF', 'MEL']
 
@@ -45,6 +6,15 @@ medium_stocks = ['WBC', 'TLS', 'AIR', 'AIA', 'SPK']
 high_stocks = [ 'CBD', 'FBU', 'SKC', 'KMD', 'GEO' ]
 
 my_portfolio = [ ]
+
+def createList():
+
+  investor_type = ['Agressive' 'Defensive' 'Balanced' 'Concervative' 'Growth']
+  money = float(input('How much money would you like to invest? \n $'))
+
+  Time = int(input('How many years are you planning on investing? \n'))
+  moneyIn(money, Time)
+
 
 def moneyIn(money, Time):
   if money > 500 and Time < 30:
@@ -55,6 +25,3 @@ def moneyIn(money, Time):
       print ("some possible investments for you could be:" + str(low_stocks))
 
 createList()
-
-
- 

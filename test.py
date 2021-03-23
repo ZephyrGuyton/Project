284ccb1e43c2975677964a91b6e1fa78f3e56c20
Zephyr Guyton <z.guyton@rollestoncollege.nz>
2021-03-24 09:40:34 +1300

    adding database test

---
+++ test.py
@@ -0,0 +1,20 @@
+
+# import the library that handles connections to sqlite databases
+import sqlite3 
+
+# Create a function
+def select_all():
+	with sqlite3.connect("db/stockmarket.db") as db:
+		cursor = db.cursor()
+		cursor.execute("select StockName,Risk,Description,Cost from stockList where Risk = 'Low' ")
+		StockLow = cursor.fetchall()
+
+	# print the entire list of medalists
+	print (StockLow)
+
+	# use a loop to print each record
+	for RiskLow in StockLow:
+		print(RiskLow)
+
+#Run the function
+select_all()

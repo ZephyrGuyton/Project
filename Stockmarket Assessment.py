475fe2d00ac8f71e159e177bf9d69a7ecd188818
Zephyr Guyton <z.guyton@rollestoncollege.nz>
2021-05-10 14:25:43 +1200

    Code linked to database

--- Stockmarket Assessment.py
+++ Stockmarket Assessment.py
@@ -3,36 +3,44 @@
 import sqlite3 
 
 # Create a function
-def LowRisk():
-	with sqlite3.connect("db/stockmarket.db") as db:
-		cursor = db.cursor()
-		cursor.execute("select StockName,Description,Cost from stockList where Risk = 'Low' ")
-		StockLow = cursor.fetchall()
-
-	# print the entire list of medalists
-	print (StockLow)
+def Risk(risk):
+  "select StockName,Description,Cost from stockList where Risk = '{0}' ".format(risk)
+  with sqlite3.connect("db/stockmarket.db") as db:
+    cursor = db.cursor()
+    cursor.execute("select StockName,Description,Cost from stockList where Risk = '{0}'".format(risk))
+    StockLow = cursor.fetchall()
 
 	# use a loop to print each record
-	for RiskLow in StockLow:
-		print(RiskLow)
+    for RiskLow in StockLow:
+      print(RiskLow)
 
 def createList():
 
-  investor_type = [ 'Agressive' 'Defensive' 'Balanced' 'Concervative' 'Growth']
-  money = float(input('How much money would you like to invest? \n $'))
+  money = int(input('How much money would you like to invest? \n $'))
   Time = int(input('How many years are you planning on investing? \n'))
   moneyIn(money, Time)
 
 
 def moneyIn(money, Time):
-  if money > 500 and Time < 30:
-    print ('Some possible investments for you could be: ' + LowRisk())
-  elif money < 500 and money > 100 and Time > 2 :
-    print ('middle')
+  print (money)
+  print (Time)
+  if money > 500 and Time < 30 and Time > 6:
+    risk='Low'
+    print ('Some possible investments for you could be:')
+    Risk(risk)
+  elif money < 500 and money > 100 and Time > 2:
+    risk='Med'
+    print ('Some possible investments for you could be:')
+    Risk(risk)
+  elif money > 500 and Time > 2 and Time < 6:
+    risk='High'
+    print ('Some possible investments for you could be:')
+    Risk(risk)
   else:
-      print('Some possible investments for you could be: ARB, ASD, ASF,\nAIA & AIR')
+    risk='Other'
+    print('Some possible investments for you could be:')
+    Risk(risk)
+
 
 createList()
 
-#Run the function
-LowRisk()
